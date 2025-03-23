5c87ac55bc0191c8ca3cbc0b9bdaa245cdfe4151
Bojan Bunić <bojan@bunic.info>
2025-03-23 10:48:11 +0100

    word count

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 book_path = "books/frankenstein.txt"
 def main():
-    print(get_book_text(book_path))
-    
+    result = split_words(get_book_text(book_path))
+    print(f"{result} words found in the document") 
 
 def get_book_text(path):
     file_contents = ""
     with open(path) as f:
         file_contents = f.read()
     return file_contents
+
+def split_words(text):
+    count = 0
+    for word in text.split():
+        count += 1
+    return count
 
 main()
